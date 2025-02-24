e047b6e8da97192c797936918de8dd193c31ea90
Maria <marder1995@mail.ru>
2025-02-24 16:52:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int amound = 13_676;
+        int number = 20;
+        int mile = amound / number;
+        System.out.println("Количество начисленных миль за купленный билет составляет:" + mile);
+
+    }
+}
