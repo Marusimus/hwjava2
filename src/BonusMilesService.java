24950cb6ec8676c733835a28291a5e5841e71c7e
Maria <marder1995@mail.ru>
2025-02-26 21:38:46 +0300

    modifired

---
+++ src/BonusMilesService.java
@@ -0,0 +1,6 @@
+public class BonusMilesService {
+    public int calculate(int amount) {
+        int mile = amount / 20;
+        return mile;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        int amound = 13_676;
-        int number = 20;
-        int mile = amound / number;
-        System.out.println("Количество начисленных миль за купленный билет составляет:" + mile);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 2000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
